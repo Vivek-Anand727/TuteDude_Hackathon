c306d4b276a09354c23fbb05eacfb606cdd52808
Vivek-Anand727 <kamjorkismatwala@gmail.com>
2025-07-26 17:53:48 +0530

    firebase removed

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const requestController = require('../controllers/requestController');
+const authenticate = require('../middleware/auth');
+const checkRole = require('../middleware/roleMiddleware');
 
-// Post a new request (by buyer or group leader)
-router.post('/create', (req, res) => {
-  // controller: requestController.createRequest
-});
+// Post a new request (only vendors or group leaders allowed)
+router.post(
+  '/create',
+  authenticate,
+  checkRole('vendor'), // or 'leader' if you have that role separately
+  requestController.createRequest
+);
 
-// Get all requests created by current user or their group
-router.get('/my-requests', (req, res) => {
-  // controller: requestController.getMyRequests
-});
+// Get all requests created by current user or their groups
+router.get(
+  '/my-requests',
+  authenticate,
+  requestController.getMyRequests
+);
 
 // Get all requests for a specific group
-router.get('/group/:groupId', (req, res) => {
-  // controller: requestController.getRequestsByGroup
-});
+router.get(
+  '/group/:groupId',
+  authenticate,
+  requestController.getRequestsByGroup
+);
 
-// (Optional) Delete a request
-router.delete('/:requestId', (req, res) => {
-  // controller: requestController.deleteRequest
-});
+// Delete a request (only if created by the user)
+router.delete(
+  '/:requestId',
+  authenticate,
+  requestController.deleteRequest
+);
 
 module.exports = router;
